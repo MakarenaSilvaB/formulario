e0988d11b883e467423ac378aa74b44e8b871504
Makarena Silva <makarena.silva@alumnos.uv.cl>
2020-11-24 17:52:20 -0300

    Actualizando contenido

--- src/Home.js
+++ src/Home.js
@@ -6,8 +6,8 @@ export const Home = (props) => (
   <div className="App">
     <img src={home} alt="pic" className="new-line"/>
     <h1 className="new-line">Bienvenido</h1>
-    <p>Actualmente estás visitando mi pequeño portfolio sobre los trabajos</p>
+    <p>Actualmente estás visitando mi pequeño sitio que contiene todos los trabajos</p>
     <p>realizados para las asignaturas de Arquitectura de Software y Desarrollo Web.</p>
-    <p>Navega en el menú lateral, para comprobar cada uno de ellos.</p>
+    <p>Navega en el menú superior, para comprobar cada uno de ellos.</p>
   </div>
 )
